11154c3c0ed0e4fce5015b453750176872d6604e
musab-Abdallah <mosabkongfu2016@gmail.com>
2024-10-19 05:48:55 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+function imgslider(anythings) {
+    document.querySelector('.blackone').src =anythings;
+}
+function changeBgColor(color) {
+  const sec = document.querySelector('.sec');
+  sec.style.background = color;
+}
+function menuToggle () {
+  const togglemenu = document.querySelector('.togglemenu');
+  const navigation = document.querySelector('.navigation');
+  togglemenu.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
+VanillaTilt.init(document.querySelectorAll(".box"), {
+  max: 25,
+  speed: 400
+});
+
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+new WOW().init();
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
